b6d6942307abfcc0c6af4547b3023ae1606249fa
Ahmad L <sc.ahmad98@gmail.com>
2026-08-07 16:15:26 +0800

    feat: add format Temperature function

--- beginner/temperature-converter/script.js
+++ beginner/temperature-converter/script.js
@@ -6,8 +6,18 @@ function fahrenheitToCelcius(fahrenheit) {
     return ((fahrenheit -32) * 5) / 9;
 }
 
+function formatTemperature(value, unit) {
+    return `${value} ${unit}`;
+}
+
 const fahrenheit = celsiusToFahrenheit(36);
 console.log(fahrenheit);
 
 const celsius = fahrenheitToCelcius(100);
 console.log(celsius);
+
+const formatedCelcius = fahrenheitToCelcius(113);
+console.log(formatTemperature(formatedCelcius, 'C'));
+
+const formatedFahrenheit = celsiusToFahrenheit(10);
+console.log(formatTemperature(formatedFahrenheit, 'F'));
