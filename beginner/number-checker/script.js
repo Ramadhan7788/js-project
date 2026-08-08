b381d8ab6133f1de25a446cfef9931900f999ba1
Ahmad L <sc.ahmad98@gmail.com>
2026-08-08 21:02:10 +0800

    feat: Create functions to check for even or odd numbers

--- beginner/number-checker/script.js
+++ beginner/number-checker/script.js
@@ -6,7 +6,23 @@ function isNegative(number) {
     return number <0;
 }
 
+function isZero(number) {
+    return number === 0;
+}
+
+function isEven(number) {
+  return number % 2 === 0;
+}
+
 const positive = 5;
-const negative =-2;
+const negative = -2;
+const zero = 0;
+const even = 8;
+const odd = 9;
+
+
 console.log(isPositive(positive));
 console.log(isNegative(negative));
+console.log(isZero(zero));
+console.log(isEven(even));
+console.log(!isEven(odd));
