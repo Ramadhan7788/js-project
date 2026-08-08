8c62cecc868dda9ffda6585442d2d70d7fe0c6f2
Ahmad L <sc.ahmad98@gmail.com>
2026-08-08 20:53:31 +0800

    feat: Create functions to check for negative and positive numbers

---
+++ beginner/number-checker/script.js
@@ -0,0 +1,12 @@
+function isPositive(number) {
+    return number > 0;
+}
+
+function isNegative(number) {
+    return number <0;
+}
+
+const positive = 5;
+const negative =-2;
+console.log(isPositive(positive));
+console.log(isNegative(negative));
