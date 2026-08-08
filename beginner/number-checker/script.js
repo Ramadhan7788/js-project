9f3332cd6bdc1f01cd206817d0f9efa22726209d
Ahmad L <sc.ahmad98@gmail.com>
2026-08-08 21:07:53 +0800

    feat: Create the last function describe Number to check whether the number is positive negative zero odd or even

--- beginner/number-checker/script.js
+++ beginner/number-checker/script.js
@@ -14,15 +14,30 @@ function isEven(number) {
   return number % 2 === 0;
 }
 
+
+function describeNumber(number) {
+    return {
+        positive: isPositive(number),
+        negative: isNegative(number),
+        zero: isZero(number),
+        even: isEven(number),
+        odd: !isEven(number),
+    };
+}
+
 const positive = 5;
 const negative = -2;
 const zero = 0;
 const even = 8;
 const odd = 9;
 
-
 console.log(isPositive(positive));
 console.log(isNegative(negative));
 console.log(isZero(zero));
 console.log(isEven(even));
 console.log(!isEven(odd));
+
+console.log(describeNumber(8));
+console.log(describeNumber(-3));
+console.log(describeNumber(0));
+console.log(describeNumber(7));
